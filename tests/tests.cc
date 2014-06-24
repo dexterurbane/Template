a0ac1c49018b3be2a8a96f71b2b729ac2400199b
Dan Gordon <dan.gordon@nicta.com.au>
2014-06-24 10:34:11 +1000

    Improve test template.

--- tests/tests.cc
+++ tests/tests.cc
@@ -1,11 +1,16 @@
 #define BOOST_TEST_MODULE test_template
 #include <boost/test/included/unit_test.hpp>
 
-BOOST_AUTO_TEST_SUITE(tests) // Name of test suite is test_template.
+// BOOST_AUTO_TEST_SUITE(tests) // Name of test suite is test_template.
 
-BOOST_AUTO_TEST_CASE(test_dummy)
+BOOST_AUTO_TEST_CASE(test_dummy_a)
 {
    BOOST_CHECK(1 == 1);
 }
 
-BOOST_AUTO_TEST_SUITE_END( )
+BOOST_AUTO_TEST_CASE(test_dummy_b)
+{
+   BOOST_CHECK(1 == 1);
+}
+
+// BOOST_AUTO_TEST_SUITE_END( )
